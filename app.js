0ab51131ef4e6b9e614eed544aa92ea3847ec30d
ghulamali17 <ghulamali47092@gmail.com>
2024-08-25 23:47:53 -0700

    error fixed

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 let cel=document.getElementById("cel");
 cel.addEventListener('input', function(){
     let c=this.value;
-    let calValue=(c * 9/5) + 32;
+    let con=(c * 9/5) + 32;
     if(!Number.isInteger(con)){
-        calValue=calValue.toFixed(4);
+        con=con.toFixed(4);
     }
     fah.value=con;
 });
 let fah=document.getElementById("fah");
 fah.addEventListener('input', function(){
     let f=this.value;
-    let calValue=(f-32) * 5/9 ;
+    let con=(f-32) * 5/9 ;
     if(!Number.isInteger(con)){
-        calValue=calValue.toFixed(4);
+        con=con.toFixed(4);
     }
     cel.value=con;
 });
